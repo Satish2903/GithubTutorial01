40317881e29acf5702e3e09a4674da765e6c95de
satish <gatthewars@gmail.com>
2026-01-09 21:52:00 +0530

    changed something -TL

--- GitHubTutorialProject/src/main/java/com/nit/model/Student.java
+++ GitHubTutorialProject/src/main/java/com/nit/model/Student.java
@@ -11,6 +11,9 @@ public class Student {
 		System.out.println("Roll no: "+rollno);
 		System.out.println("Name: "+name);
 		System.out.println("Marks: "+marks);
+		System.out.println("--------------------------------");
 	}
+	
+	
 	
 }
