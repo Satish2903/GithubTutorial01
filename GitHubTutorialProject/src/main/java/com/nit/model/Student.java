c9ec3099d93572b0be44aefb2692a05c73ea9b6e
satish <gatthewars@gmail.com>
2026-01-09 20:51:04 +0530

    Model created -TL

---
+++ GitHubTutorialProject/src/main/java/com/nit/model/Student.java
@@ -0,0 +1,16 @@
+package com.nit.model;
+
+public class Student {
+
+	private Integer rollno;
+	private String name;
+	private Integer marks;
+	
+	
+	public void display() {
+		System.out.println("Roll no: "+rollno);
+		System.out.println("Name: "+name);
+		System.out.println("Marks: "+marks);
+	}
+	
+}
